518c2fbcf01b0ac12dac25f857ec58eef05daa9d
Justin Romanowski <just.roman7@gmail.com>
2017-09-14 05:00:29 -0700

    finished checkpoint-3

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,17 @@
 (function() {
-  function HomeCtrl(Room, $scope) {
+  function HomeCtrl(Room, $scope, $uibModal) {
     console.log(Room);
     $scope.Rooms = Room.all;
+    this.addRoom = function() {
+      $uibModal.open({
+        templateUrl: '/templates/modal.html',
+        size: 'sm',
+        controller: 'ModalCtrl as modal'
+      });
+    }
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', '$scope', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', '$scope', '$uibModal', HomeCtrl]);
 })();
---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,15 @@
+(function(){
+  function ModalCtrl(Room, $uibModalInstance) {
+    this.cancel = function() {
+      $uibModalInstance.dismiss();
+    };
+    this.submit = function() {
+      Room.add(this.newRoom);
+      $uibModalInstance.close();
+    };
+  }
+
+  angular
+    .module('blocChat')
+    .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+})();
