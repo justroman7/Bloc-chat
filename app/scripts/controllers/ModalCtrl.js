effe2427f3f812f1f3963f5acc4fb547f294be63
Justin Romanowski <just.roman7@gmail.com>
2017-09-21 11:52:43 -0700

    worked on checkpoint 5

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,5 +1,5 @@
 (function(){
-  function ModalCtrl(Room, $uibModalInstance) {
+  function ModalCtrl(Room, $uibModalInstance, $cookies) {
     this.cancel = function() {
       $uibModalInstance.dismiss();
     };
@@ -7,9 +7,14 @@
       Room.add(this.newRoom);
       $uibModalInstance.close();
     };
+
+    this.createUsername = function() {
+      $cookies.put('blocChatCurrentUser', this.username);
+      $uibModalInstance.close();
+    }
   }
 
   angular
     .module('blocChat')
-    .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+    .controller('ModalCtrl', ['Room', '$uibModalInstance', '$cookies', ModalCtrl]);
 })();
